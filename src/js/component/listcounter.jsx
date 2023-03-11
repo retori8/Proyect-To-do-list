243c66782e5babfad1399d6cd156f78138f6af79
retori8 <reciclee@gmail.com>
2023-03-11 20:17:46 +0000

    contador arreglado

--- src/js/component/listcounter.jsx
+++ src/js/component/listcounter.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function ListCounter(list){
+export default function ListCounter({list}){
     return(
         <li className="list-group-item align-items-start">
             <span className="badge bg-secondary rounded-pill">
-              {{list}.length} item{{list}.length >= 2 ? "s" : ""} left
+              {list.length} item{list.length >= 2 ? "s" : ""} left
             </span>
         </li>
     )
